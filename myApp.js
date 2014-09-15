91dd00e4fe7b752329042ea711c9cb7d1adbcf55
Patrick Baker <patricksbaker@gmail.com>
2014-09-15 00:45:23 -0400

    Add events

--- myApp.js
+++ myApp.js
@@ -2,7 +2,13 @@
  * Created by pbaker on 9/14/14.
  */
 angular.module('myApp', [])
-    .controller('MultiplicationCtrl', function($scope, $attrs) {
+    .controller('DisplayCtrl', function($scope) {
+        $scope.$on('displayData', function(event, data) {
+            $scope.content = data;
+        });
+    })
+
+    .controller('MultiplicationCtrl', function($scope, $attrs, $rootScope) {
         function populateNumbers(x) {
             var numbers = [];
             for(var i=0; i<x; i++) {
@@ -13,11 +19,29 @@ angular.module('myApp', [])
 
         $scope.compute = function(a,b) {
             return a * b;
-        }
+        };
 
         $scope.$watch('numberLimit', function(limit) {
             $scope.numbers = populateNumbers(limit);
         });
 
         $scope.numberLimit = $attrs.initialNumberLimit || 10;
+
+        var activeFactorA, activeFactorB;
+        $scope.setActiveFactors = function(a, b) {
+            activeFactorA = a;
+            activeFactorB = b;
+        };
+
+        $scope.matchesFactor = function (a, b) {
+            return a === activeFactorA || b === activeFactorB;
+        };
+
+        $scope.clearActiveFactors = function () {
+            activeFactorA = activeFactorB = null;
+        };
+
+        $scope.setActiveNumber = function(number) {
+            $rootScope.$broadcast('displayData', number);
+        };
     });
