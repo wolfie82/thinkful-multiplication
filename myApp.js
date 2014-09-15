a1f2f34c282704622c41dcf1f17a0a0b0aa8e801
Patrick Baker <patricksbaker@gmail.com>
2014-09-15 00:03:19 -0400

    Initial

---
+++ myApp.js
@@ -0,0 +1,7 @@
+/**
+ * Created by pbaker on 9/14/14.
+ */
+angular.module('myApp', [])
+    .controller('MultiplicationCtrl', function($scope) {
+        $scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    });
